bbe5ebea90fe334153ba3842e2bd9b857af8db82
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2024-11-10 16:15:09 +0100

    Update install-gui.sh
    - Removed useless packages
    - Ubuntu is now supported

--- install-gui.sh
+++ install-gui.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-sudo apt update
-sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
-# Download Chrome Remote Desktop, install it, then delete downloaded .deb package
+
+# Update package lists and install necessary packages based on the distribution
+if grep -q "Ubuntu" /etc/os-release; then
+    sudo apt update
+    sudo apt install -y xvfb xfce4 xfce4-goodies firefox plank papirus-icon-theme dbus-x11 neofetch
+elif grep -q "Debian" /etc/os-release; then
+    sudo apt update
+    sudo apt install -y xvfb xfce4 xfce4-goodies firefox-esr plank papirus-icon-theme dbus-x11 neofetch
+else
+    echo "Unsupported distribution"
+    exit 1
+fi
+
+# Download and install Chrome Remote Desktop
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome-remote-desktop*
-sudo apt --fix-broken install --assume-yes
-rm chrome-remote-desktop*
+sudo dpkg -i chrome-remote-desktop_current_amd64.deb
+sudo apt --fix-broken install -y
+rm chrome-remote-desktop_current_amd64.deb
+
 # Install Catppuccin theme
-sudo apt install sassc --assume-yes
+sudo apt install -y sassc
 git clone https://github.com/catppuccin/gtk.git
 cd gtk
 sudo make build
 sudo make package
 cd pkgs
-sudo cp * /usr/share/themes
+sudo cp -r * /usr/share/themes
 cd /usr/share/themes
 ~/install-catppuccin.sh
+
 # Install Catppuccin Plank theme
 cd
 git clone https://github.com/catppuccin/plank.git
